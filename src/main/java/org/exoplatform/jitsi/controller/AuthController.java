be9584279e9b48cbdfa3875f2842234a4b5ae959
VladMartyshevskyi <vladmartishevskii@gmail.com>
2020-09-01 16:32:20 +0300

    Updated Auth controller

--- src/main/java/org/exoplatform/jitsi/controller/AuthController.java
+++ src/main/java/org/exoplatform/jitsi/controller/AuthController.java
@@ -3,13 +3,18 @@ package org.exoplatform.jitsi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class AuthController {
   
-    // Auth endpoint ( will be open in iframe from eXo for setting the token to local storage )
-    @GetMapping("/auth")
-    public String index(Model model) {
-        return "auth";
-    }
+  // Auth endpoint ( will be open in iframe from eXo for setting the token to local storage )
+  @GetMapping("/auth")
+  public String index(@RequestHeader("X-Exoplatform-Identity") String identity,
+                      @RequestHeader("X-Exoplatform-Auth") String authToken,
+                      Model model) {
+    System.out.println("IDENTITY: " + identity);
+    System.out.println("AUTH: " + authToken);
+    return "auth";
+  }
 }
